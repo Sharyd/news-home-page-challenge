7d587b3d841aa5c495fbe759650c8fc8439a75b5
Dan <Danevac@seznam.cz>
2023-04-15 20:06:57 +0200

    added useMediaQuery for mobileNav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { theme } from './theme'
 import { Box, Container } from '@mui/material'
 import { ThemeProvider } from '@mui/material/styles'
 import Navbar from './components/Layout/Navbar'
 import Main from './components/Layout/Main'
-
+import useMediaQuery from '@mui/material/useMediaQuery'
 function App() {
     const [active, setActive] = useState(false)
+    const matches = useMediaQuery('(min-width:680px)')
+    useEffect(() => {
+        if (matches) setActive(false)
+    }, [matches])
+
     return (
         <ThemeProvider theme={theme}>
             <Container
